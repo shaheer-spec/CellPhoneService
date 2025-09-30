44b3adfb94919e2bc51e713ae3fb178e89cf1358
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-09-30 16:00:55 -0400

    Commit and push with code

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("What is the serial number? ");
+        int serialNumber = myScanner.nextInt();
+        myScanner.nextLine();
+        System.out.print("What model is the phone? ");
+        String model = myScanner.nextLine();
+        System.out.print("Who is the carrier? ");
+        String carrier = myScanner.nextLine();
+        System.out.print("What is the phone number? ");
+        String phoneNumber = myScanner.nextLine();
+        System.out.print("Who is the owner of the phone? ");
+        String owner = myScanner.nextLine();
+
+        CellPhone phone = new CellPhone();
+        phone.setSerialNumber(serialNumber);
+        phone.setModel(model);
+        phone.setCarrier(carrier);
+        phone.setPhoneNumber(phoneNumber);
+        phone.setOwner(owner);
+        System.out.println("====================================================");
+
+        System.out.println("The serial number: " + phone.getSerialNumber());
+        System.out.println("The model: " + phone.getModel());
+        System.out.println("The carrier: " + phone.getCarrier());
+        System.out.println("The phone number: " + phone.getPhoneNumber());
+        System.out.println("The owner of the phone: " + phone.getOwner());
+    }
 }
